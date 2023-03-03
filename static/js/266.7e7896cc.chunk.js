f08a4979a3af5fdd8ca5812950100241af759300
Reshte <107108359+Reshte@users.noreply.github.com>
2023-03-03 14:38:56 +0000

    Deploying to gh-pages from @ Reshte/goit-react-hw-08-phonebook@f95081f0b137808f3bdf54dd256fb29e57b7a650 🚀

--- static/js/266.7e7896cc.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(184),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:r.container,children:(0,i.jsx)("h1",{style:r.title,children:"Your favorite phonebook"})})}}}]);
-//# sourceMappingURL=266.7e7896cc.chunk.js.map
